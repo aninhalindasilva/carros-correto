c2fd51abd5a56160e4ae30fb37d43d5cae93de55
ana clara gomes da silva <aninhagomessilva6@gmail.com>
2024-03-20 20:01:00 -0300

    quarto commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,19 @@ export default function App(){
       <section>
         <div className="bg-orange">
             <img src={iconSedans} alt="Ícone de carros sedan"/>
-            <h2 className="text_xl">Sedans</h2>
-            <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem</p>
+            <h2 className="font-showlders">Sedans</h2>
+            <p className="font-lexend">Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem</p>
         <a href="https://www.webmotors.com.br/sedans/carros/estoque?estadocidade=estoque&necessidade=Sedans&idcmpint=t1:c17:m07:portal-ofertas:busque-por-categorias::sedans:comprar" target="_blank">Ver mais</a>
         </div>
         <div className="bg-green">
             <img src={iconSuvs} alt="Ícone dos carros SUV" />
-            <h2>SUvs</h2>
-            <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
+            <h2 className="font-showlders">SUvs</h2>
+            <p className="font-lexend">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
             <a href="https://www.webmotors.com.br/suvs/carros/estoque?estadocidade=estoque&necessidade=SUVs&idcmpint=t1:c17:m07:portal-ofertas:busque-por-categorias::suvs:comprar" target="_blank">Ver mais</a>
             <div className="bg-darck-green">
             <img src={iconLuxury} alt="Ícone dos carros de luxos" />
-            <h2>Luxuosos</h2>
-            <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
+            <h2 className="font-showlders" >Luxuosos</h2>
+            <p className="font-lexend">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
             <a href="https://www.webmotors.com.br/carros-de-luxo/carros/estoque?estadocidade=estoque&necessidade=Carros%20de%20luxo&idcmpint=t1:c17:m07:portal-ofertas:busque-por-categorias::carros-de-luxo:comprar" target="_blank">Ver mais</a>
             </div>
 
